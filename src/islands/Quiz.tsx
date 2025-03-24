d4c5310f5e0e299e09e3af8e2b41efa701abac8e
Endy3032 <endy3032@icloud.com>
2025-03-25 00:05:34 +0700

    sort wrong questions to top

--- src/islands/Quiz.tsx
+++ src/islands/Quiz.tsx
@@ -226,37 +226,44 @@ export function Quiz(props: { questions: Question[] }) {
 			)
 			: (
 				<div className="flex flex-col gap-4 max-w-screen-lg mx-auto">
-					{questions.map((q, i) => (
-						<div key={i} class="flex flex-col gap-3 p-4 rounded-lg border border-zinc-700 shadow-md w-full">
-							<h2 class="text-xl whitespace-pre-wrap font-semibold leading-snug">
-								<span class="float-right ms-2 mb-2 text-sm text-zinc-400">#{q.id}</span>
-								<span>{q.question}</span>
-							</h2>
-							{q.image && <img src={q.image} alt="Question Image" class="rounded-md max-w-[min(32rem,100%)] max-h-[32rem] mx-auto" />}
-							<ul className="grid grid-cols-1 md:grid-cols-2 gap-2">
-								{q.choices?.map((choice, ci) => (
-									<li key={ci} class={cn(
-										"p-2 rounded",
-										q.answer?.includes(ci + 1)
-											? colorblind.value ? "bg-cyan-700" : "bg-emerald-700"
-											: optionCounts.value[i][ci]
-											? "bg-yellow-700"
-											: "bg-zinc-700",
-									)}>
-										<span>{choice}</span>
-										<span className="float-right text-zinc-400 font-medium">x{optionCounts.value[i][ci]}</span>
-									</li>
-								))}
-							</ul>
-							{q.explanation && (
-								<div class="flex flex-col gap-2">
-									{q.explanation.split("\n").map((line, i) => (
-										<p key={i} class="text-zinc-400 whitespace-pre-wrap leading-snug">{line}</p>
+					{questions
+						.map((q, i) => (
+							<div key={i} class="flex flex-col gap-3 p-4 rounded-lg border border-zinc-700 shadow-md w-full">
+								<h2 class="text-xl whitespace-pre-wrap font-semibold leading-snug">
+									<span class="float-right ms-2 mb-2 text-sm text-zinc-400">#{q.id}</span>
+									<span>{q.question}</span>
+								</h2>
+								{q.image && <img src={q.image} alt="Question Image"
+									class="rounded-md max-w-[min(32rem,100%)] max-h-[32rem] mx-auto" />}
+								<ul className="grid grid-cols-1 md:grid-cols-2 gap-2">
+									{q.choices?.map((choice, ci) => (
+										<li key={ci} class={cn(
+											"p-2 rounded",
+											q.answer?.includes(ci + 1)
+												? colorblind.value ? "bg-cyan-700" : "bg-emerald-700"
+												: optionCounts.value[i][ci]
+												? "bg-yellow-700"
+												: "bg-zinc-700",
+										)}>
+											<span>{choice}</span>
+											<span className="float-right text-zinc-400 font-medium">x{optionCounts.value[i][ci]}</span>
+										</li>
 									))}
-								</div>
-							)}
-						</div>
-					))}
+								</ul>
+								{q.explanation && (
+									<div class="flex flex-col gap-2">
+										{q.explanation.split("\n").map((line, i) => (
+											<p key={i} class="text-zinc-400 whitespace-pre-wrap leading-snug">{line}</p>
+										))}
+									</div>
+								)}
+							</div>
+						))
+						.sort((a, b) => {
+							const aCorrect = optionCounts.value[a.key].reduce((acc, cur) => acc + cur, 0) === questions[a.key].answer?.length
+							const bCorrect = optionCounts.value[b.key].reduce((acc, cur) => acc + cur, 0) === questions[b.key].answer?.length
+							return aCorrect && !bCorrect ? 1 : !aCorrect && bCorrect ? -1 : 0
+						})}
 				</div>
 			)
 	)
